d36c796136f2e06dddbd50dc9a1488704e7f2e27
Sylvia Khuu <khuu.sylvia@gmail.com>
2022-03-17 09:53:58 +0100

    Change baseMvi

--- app/src/main/java/com/example/fizzbuzz/core/BaseMVIViewModel.kt
+++ app/src/main/java/com/example/fizzbuzz/core/BaseMVIViewModel.kt
@@ -1,40 +1,46 @@
 package com.example.fizzbuzz.core
 
 import android.util.Log
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.SavedStateHandle
-import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.BufferOverflow
 import kotlinx.coroutines.flow.*
-import kotlinx.coroutines.flow.SharingStarted.Companion.WhileSubscribed
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.SharingStarted.Companion.Eagerly
 
-public abstract class BaseMVIViewModel<S : MVI.State, I : MVI.Intent>(
+abstract class BaseMVIViewModel<S : MVI.State, I : MVI.Intent>(
     private val firstModel: S,
+    dispatcher: CoroutineDispatcher = Dispatchers.Main.immediate,
 ) : androidx.lifecycle.ViewModel(),
     MVI.ViewModel<S, I> {
 
-    protected val currentState: S get() = _uiState.replayCache.firstOrNull() ?: firstModel
+    val mviScope: CoroutineScope = viewModelScope + dispatcher
 
-    private val _uiState = MutableSharedFlow<S>(replay = 1, extraBufferCapacity = 64)
+    private var _currentState: S = firstModel
+    protected val currentState: S get() = _currentState
+
+    private val _uiState = MutableSharedFlow<S>(
+        replay = 1,
+        extraBufferCapacity = 64,
+        onBufferOverflow = BufferOverflow.SUSPEND
+    )
 
     /**
      * Flow that keeps track of the UI State
      */
-    public val uiState: SharedFlow<S> =
-        _uiState.onStart { emit(firstModel) }
-            .onEach { log("new state: $it") }
-            .shareIn(
-                scope = viewModelScope,
-                started = WhileSubscribed(5000),
-                replay = 1
-            )
+    val uiState: SharedFlow<S> =
+        _uiState.onStart {
+            emit(firstModel)
+        }.onEach {
+            log("state consumed: $it")
+        }.shareIn(
+            scope = mviScope,
+            started = Eagerly,
+            replay = 1
+        )
 
-    private val _uiEvent: MutableSharedFlow<MVI.Event> = MutableSharedFlow<MVI.Event>(
+    private val _uiEvent: MutableSharedFlow<MVI.Event> = MutableSharedFlow(
         extraBufferCapacity = 1,
-    ).apply {
-        onEach { log("new event: $it") }
-    }
+    )
 
     /**
      * Flow that pop one-shot events
@@ -47,29 +53,35 @@ public abstract class BaseMVIViewModel<S : MVI.State, I : MVI.Intent>(
      * Publishes a new state
      */
     protected fun publishState(state: S) {
-        viewModelScope.launch { _uiState.emit(state) }
+        val published = _uiState.tryEmit(state)
+        if (published) {
+            _currentState = state
+        }
     }
 
     /**
      * Emits a one-shot event
      */
     protected fun emitEvent(event: MVI.Event) {
-        viewModelScope.launch {
+        mviScope.launch {
             _uiEvent.emit(event)
+            log("event emitted: $event")
         }
     }
 
-    public fun <P : MVI.State> reduceAndPublishState(reducer: MVI.Reducer<S, P>, partial: P) {
-        viewModelScope.launch {
-            _uiState.emit(reducer.reduce(currentState, partial))
+    fun <P : MVI.State> reduceAndPublishState(reducer: MVI.Reducer<S, P>, partial: P) {
+        val newState = reducer.reduce(currentState, partial)
+        val published = _uiState.tryEmit(newState)
+        if (published) {
+            _currentState = newState
         }
     }
 
     protected abstract suspend fun processIntent(intent: I)
 
     final override fun sendIntent(intent: I) {
-        log("new intent: $intent")
-        viewModelScope.launch {
+        mviScope.launch {
+            log("process intent: $intent")
             processIntent(intent)
         }
     }
@@ -77,20 +89,20 @@ public abstract class BaseMVIViewModel<S : MVI.State, I : MVI.Intent>(
     private fun log(message: String) = Log.d(this::class.java.simpleName, message)
 }
 
-public object MVI {
+object MVI {
 
-    public interface State
+    interface State
 
-    public interface Intent
+    interface Intent
 
-    public interface Event
+    interface Event
 
-    public interface ViewModel<S : State, I : Intent> {
+    interface ViewModel<S : State, I : Intent> {
 
-        public fun sendIntent(intent: I)
+        fun sendIntent(intent: I)
     }
 
-    public fun interface Reducer<S : State, P : State> {
-        public fun reduce(previousState: S, partialState: P): S
+    fun interface Reducer<S : State, P : State> {
+        fun reduce(previousState: S, partialState: P): S
     }
 }
